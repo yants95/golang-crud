e37082399b7dc8561809dff05fbd4bb83f67421c
Yan Soares <yts.1995@gmail.com>
2024-07-01 11:54:47 -0300

    chore: initializing go project

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	fmt.Println(os.Getenv("TEST"))
+}
