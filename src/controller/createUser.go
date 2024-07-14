cf37a185d8cd223f501660d29021828a8c544858
Yan Soares <yts.1995@gmail.com>
2024-07-14 17:01:06 -0300

    feat: add user_request and user_response

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,5 +1,22 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
 
-func CreateUser(c *gin.Context) {}
+	"github.com/gin-gonic/gin"
+	"github.com/yants95/goland-crud/src/configuration/rest_err"
+	"github.com/yants95/goland-crud/src/controller/model/request"
+)
+
+func CreateUser(c *gin.Context) {
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields, error=%s", err.Error))
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println((userRequest))
+}
